9ef462faa285373a659128af75d2212b1ae458b4
kuyaboy <hanspeter.pleisch@outlook.com>
2024-12-01 11:22:38 +0100

    added pattern for re.match for Yen

--- src/helper/filter_duplicate_prices.py
+++ src/helper/filter_duplicate_prices.py
@@ -1,7 +1,5 @@
 import regex as re
 
-import re
-
 def filter_unique_prices(original_shipping_prices_list):
     filtered_prices = []
     i = 0
@@ -9,8 +7,8 @@ def filter_unique_prices(original_shipping_prices_list):
     while i < len(original_shipping_prices_list):
         current_price = original_shipping_prices_list[i]
 
-        # Check if the current price is numeric using regex
-        if re.match(r'^\d+\.\d+$', current_price):
+        # Check if the current price matches either numeric format with a period or a comma
+        if re.match(r'^\d+\.\d+$', current_price) or re.match(r'^\d+\,\d+$', current_price):
             # Check if the next price is the same (i.e., a pair)
             if i + 1 < len(original_shipping_prices_list) and original_shipping_prices_list[i + 1] == current_price:
                 # Add the price once if it's part of a pair
