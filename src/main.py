7880dacab1bf19c9eac8cca879bca2bf69c664de
kuyaboy <hanspeter.pleisch@outlook.com>
2024-12-03 23:03:23 +0100

    added price conversion function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
+from src.scripts.convert_currency import convert_price_to_chf
 from src.scripts.convert_discogs_xml import convert_discogs_html_to_xml
 from src.scripts.convert_xml_to_dict import convert_wantlist_xml_to_dict
 from src.scripts.import_to_mongodb import import_listings_to_mongodb
 
+from dotenv import load_dotenv
+
+
+load_dotenv(override=True)
 
 def main():
 
     convert_discogs_html_to_xml()
     convert_wantlist_xml_to_dict()
+    convert_price_to_chf()
     import_listings_to_mongodb()
 
 
