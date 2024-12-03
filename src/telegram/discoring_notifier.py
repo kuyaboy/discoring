3d41254aa17eebef576c65d0bc12b762a2c260a8
kuyaboy <hanspeter.pleisch@outlook.com>
2024-12-03 23:02:04 +0100

    added telegram notifier

---
+++ src/telegram/discoring_notifier.py
@@ -0,0 +1,61 @@
+# https://api.telegram.org/bot8030423215:AAHwsPTMVHUs8pMscH07bn03O2y-gcAVlHE/getUpdates --> get updates about my groupchat
+# https://api.telegram.org/bot8030423215:AAHwsPTMVHUs8pMscH07bn03O2y-gcAVlHE/sendMessage?chat_id=-4699405740&text="This is my first message" --> send message to my group chat
+
+import os
+import requests
+import urllib
+from src.mongodb.database import Database
+from src.mongodb.queries import get_query_les_nubiennes
+from src.mongodb.queries import get_query_return_of_the_devils_son
+from src.mongodb.queries import get_query_roads_out_the_ghetto
+from src.mongodb.queries import get_query_sun_kissed_lady
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+def check_and_notify():
+    mongodb = Database()
+    mongodb.inititalize()
+
+    collection = os.getenv('MONGODB_COLLECTION')
+    chat_url = os.getenv('TELEGRAM_CHAT_URL')
+    sell_item_url = 'https://www.discogs.com/sell/item/'
+
+    queries = [
+        get_query_les_nubiennes,
+        get_query_return_of_the_devils_son,
+        get_query_roads_out_the_ghetto,
+        get_query_sun_kissed_lady
+    ]
+
+    query_results = []
+
+    for get_query in queries:
+        query = get_query()
+        results = mongodb.find(collection, query)
+        results_list = list(results)
+        query_results.extend(results_list)
+
+    if query_results:
+        for listing_id, record_name, record_price, shipping_price in zip(
+                [result['listing_id'] for result in query_results],
+                [result['record_name'] for result in query_results],
+                [result['item_price_chf'] for result in query_results],
+                [result['shipping_price_chf'] for result in query_results]
+        ):
+
+            text = f"""*!!ALERT!!* 🚨🚨🚨
+
+*Record Name:* {record_name}
+*Price (CHF):* {record_price}
+*Shipping Price (CHF):* {shipping_price}
+
+*Link to Record:* [Click here]({sell_item_url + listing_id})
+"""
+
+            encoded_text = urllib.parse.quote_plus(text)
+
+            requests.get(f"{chat_url}{encoded_text}", params={"parse_mode": "Markdown"})
+
+
