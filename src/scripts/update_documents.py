13a6eeef2e8bc2ea76e245a41b01ea4e48f83b72
kuyaboy <hanspeter.pleisch@outlook.com>
2025-03-30 17:09:52 +0200

    fixed error

--- src/scripts/update_documents.py
+++ src/scripts/update_documents.py
@@ -1,6 +1,9 @@
 import os
 import json
 from mongodb.database import Database  # Assuming you have a Database class handling MongoDB connections
+from logger import get_logger
+
+logger = get_logger()
 
 def update_listings_in_mongodb():
     directory = os.path.join(os.getcwd(), 'src', 'data', 'listings_json')
@@ -19,21 +22,26 @@ def update_listings_in_mongodb():
         for record in data:
             listing_id = record['listing_id']
             seller_name = record['seller_name']
+            record_name = record['record_name']
 
             existing_entry = mongodb.find_one(collection, {'listing_id': listing_id, 'seller_name': seller_name})
 
             if existing_entry:
                 # Check if any of the relevant fields have changed from the collection compared to the new dictionary
-                if (existing_entry.get('item_price') != record['item_price'] or
-                    existing_entry.get('shipping_price') != record['shipping_price'] or
-                    existing_entry.get('media_condition') != record['media_condition'] or
-                    existing_entry.get('sleeve_condition') != record['sleeve_condition']):
+                if (existing_entry.get('item_price') != record.get('item_price') or
+                    existing_entry.get('shipping_price') != record.get('shipping_price') or
+                    existing_entry.get('media_condition') != record.get('media_condition') or
+                    existing_entry.get('sleeve_condition') != record.get('sleeve_condition')):
 
                     # Update the document only if a field has changed
                     update_query = {'listing_id': listing_id, 'seller_name': seller_name}
                     update_data = {'$set': record}
-
+                    logger.debug(f'Updating: {record}')
                     mongodb.update_one(collection, update_query, update_data, upsert=True)
+                    logger.info(f'Successfully updated: {record}')
+
+                else:
+                    logger.info(f'No listings to update for {record_name}')
 
 if __name__ == "__main__":
     update_listings_in_mongodb()
