378760db328defd351d3b9c71982d75fd399c41f
kuyaboy <hanspeter.pleisch@outlook.com>
2025-05-17 13:18:11 +0200

    adjust get_wantlist method to only get full wantlist

--- src/discogsclient/wantlist.py
+++ src/discogsclient/wantlist.py
@@ -24,19 +24,14 @@ class DiscogsWantlistClient:
         except Exception as e:
             logger.error(f'Error during Discogs authorization process: {e}')
 
+
     def get_wantlist(self):
 
         username = os.getenv('DISCOGS_USERNAME')
         user = self.client.user(username)
-        wantlist = user.wantlist
-        # don't create  a dictionary for each record (with increasing numbers of entries in a wantlist this will gonna take a while, do it based ont he filter_config)
-        wantlist_dict = {record.release.id: {'title': record.release.title,
-                                             'genre': record.release.genres,
-                                             'artist': [artist.name for artist in record.release.artists],
-                                             'label': [label.name for label in record.release.labels],
-                                             'year': record.release.year,
-                                             'format': record.release.formats,
-                                             'country': record.release.country}
-                         for record in wantlist}
 
-        return wantlist_dict
+        wantlist = user.wantlist
+        wantlist_data = [record.data for record in wantlist]
+
+        return wantlist_data
+
