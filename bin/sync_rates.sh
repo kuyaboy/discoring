4b7b8cde79d3bbf5b289d5d667f162ba8f4a4bc2
kuyaboy <hanspeter.pleisch@outlook.com>
2025-05-19 21:23:55 +0200

    minor pathing error fix

--- bin/sync_rates.sh
+++ bin/sync_rates.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -o allexport
 source .env
 set +o allexport
 
 CURRENCY="CHF"
-FXRATES_API_URL="https://api.fxratesapi.com?api_key=$FXRATES_API_TOKEN&base=$CURRENCY&places=2"
+FXRATES_API_URL="https://api.fxratesapi.com/latest?api_key=$FXRATES_API_TOKEN&base=$CURRENCY&places=2"
 OUTPUT_FILE="src/config/exchange_rates.json"
 STATUS_CODE=$(curl -s -o /dev/null -w "%{response_code}" "$FXRATES_API_URL")
 ERROR_MESSAGE="ERROR: Could not sync exchange rate - $STATUS_CODE"
