d73d489af8ca25f50478ee4442fffa54d1c6d677
kuyaboy <hanspeter.pleisch@outlook.com>
2025-05-19 21:46:26 +0200

    moved currency setting to .env file

--- src/mongodb/queries.py
+++ src/mongodb/queries.py
@@ -1,10 +1,12 @@
+import os
+
 # define query template
-def create_query(record_name, item_price_chf, shipping_price_chf):
+def create_query(record_name, item_price_currency, shipping_price_currency):
     return {
         '$and': [
             {'record_name': {'$eq': record_name}},
-            {'item_price_chf': {'$lte': item_price_chf}},
-            {'shipping_price_chf': {'$lte': shipping_price_chf}},
+            {f'item_price_{os.getenv('CURRENCY')}': {'$lte': item_price_currency}},
+            {f'shipping_price_{os.getenv('CURRENCY')}': {'$lte': shipping_price_currency}},
             {'media_condition': {'$in': ('Very Good Plus (VG+)', 'Near Mint (NM or M-)', 'Mint (M)')}},
             {'sleeve_condition': {'$in': ('Very Good Plus (VG+)', 'Near Mint (NM or M-)', 'Mint (M)')}},
         ]
@@ -13,5 +15,5 @@ def create_query(record_name, item_price_chf, shipping_price_chf):
 def get_queries():
     return {
         'princesses_nubiennes': create_query('Princesses Nubiennes', 80, 80),
-        'butter_smoother': create_query('今すぐ欲しい', 45, 15)
+        'butter_smoother': create_query('今すぐ欲しい', 45, 50)
     }
--- src/scripts/convert_currency.py
+++ src/scripts/convert_currency.py
@@ -29,14 +29,14 @@ def convert_price_to_chf():
 
             if listings[item]['currency'] in rates['rates']:
                 exchange_rate = float(rates['rates'][currency])
-                listings[item]['item_price_chf'] = round(float(listings[item]['item_price']) * (1 / exchange_rate), 2)
+                listings[item][f'item_price_{os.getenv('CURRENCY')}'] = round(float(listings[item]['item_price']) * (1 / exchange_rate), 2)
             else:
-                listings[item]['item_price_chf'] = "NaN"
+                listings[item][f'item_price_{os.getenv('CURRENCY')}'] = "NaN"
 
             if re.match(r'^\d+\.\d+$', shipping_price) or re.match(r'^\d+$', shipping_price):
-                listings[item]['shipping_price_chf'] = round(float(listings[item]['shipping_price']) * (1 / exchange_rate), 2)
+                listings[item][f'shipping_price_{os.getenv('CURRENCY')}'] = round(float(listings[item]['shipping_price']) * (1 / exchange_rate), 2)
             else:
-                listings[item]['shipping_price_chf'] = "NaN"
+                listings[item][f'shipping_price_{os.getenv('CURRENCY')}'] = "NaN"
 
         with open(file_path, 'w', encoding='utf-8') as listing_file:
             json.dump(listings, listing_file, ensure_ascii=False, indent=2)
