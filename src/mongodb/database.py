7c996777fa36a45c387bf3e09ab5f6819aa3d17d
kuyaboy <hanspeter.pleisch@outlook.com>
2025-05-20 21:30:26 +0200

    added close database method

--- src/mongodb/database.py
+++ src/mongodb/database.py
@@ -2,59 +2,57 @@ import os
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+
 from src.logger import get_logger
 
 logger = get_logger()
 
-class Database(object):
-    URI = os.getenv('MONGODB_URI')
-    DATABASE = None
-
+class Database:
+    client = None
+    db = None
 
     @staticmethod
     def initialize():
-        client = MongoClient(Database.URI, server_api=ServerApi('1'))
-
         try:
-            client.admin.command('ping')
-            Database.DATABASE = client[os.getenv('MONGODB_NAME')]
+            Database.client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
+            Database.client.admin.command('ping')
+            Database.db = Database.client[os.getenv('MONGODB_NAME')]
             logger.info('Successfully connected to MongoDB')
-
         except Exception as e:
             logger.error(f'Failed to connect to MongoDB: {e}')
             raise
 
-
     @staticmethod
     def insert(collection, data):
-        Database.DATABASE[collection].insert_one(data)
-
+        Database.db[collection].insert_one(data)
 
     @staticmethod
     def find(collection, query):
-        return Database.DATABASE[collection].find(query)
-
+        return Database.db[collection].find(query)
 
     @staticmethod
     def find_one(collection, query):
-        return Database.DATABASE[collection].find_one(query)
-
+        return Database.db[collection].find_one(query)
 
     @staticmethod
     def find_release_id(collection):
-        return Database.DATABASE[collection].distinct("release_id")
-
+        return Database.db[collection].distinct("release_id")
 
     @staticmethod
     def update_one(collection, query, update, upsert=False):
-        Database.DATABASE[collection].update_one(query, update, upsert=upsert)
-
+        Database.db[collection].update_one(query, update, upsert=upsert)
 
     @staticmethod
     def delete_many(collection, query):
-        Database.DATABASE[collection].delete_many(query)
-
+        Database.db[collection].delete_many(query)
 
     @staticmethod
     def delete_all_documents(collection):
-        Database.DATABASE[collection].delete_many({})
+        Database.db[collection].delete_many({})
+        logger.info(f'Deleted all documents from {collection}')
+
+    @staticmethod
+    def close():
+        if Database.client:
+            Database.client.close()
+            logger.info('Successfully closed connection to MongoDB')
